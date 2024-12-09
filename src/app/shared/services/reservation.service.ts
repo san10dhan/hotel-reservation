84160fc87ee5bca3602157581b783e7d7412443b
Sanket <san10dhan@gmail.com>
2024-12-09 10:51:36 +0530

    creating reservations service

--- src/app/shared/services/reservation.service.ts
+++ src/app/shared/services/reservation.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../../models/reservation';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
-
+  private reservations: Reservation[] = [];
   constructor() { }
+
+  getAllResevations(): Reservation[] {
+    return this.reservations;
+  }
+
+  getReservationById(id: number): Reservation | undefined{
+    return this.reservations.find(res => res.id === id);
+  }
+
+  deleteResrvation(id: number): void {
+    let index = this.reservations.findIndex(res => res.id === id);
+    this.reservations.splice(index, 1);
+  }
+
+  addReservation(reservation: Reservation): void {
+    this.reservations.push(reservation);
+  }
+
+  updateReservaton(updatedReservation: Reservation): void {
+    let index = this.reservations.findIndex(res => res.id === updatedReservation.id);
+    this.reservations[index] = updatedReservation;
+  }
 }
